72c1c47ab2eee7afea8237beb2e1f72c287fde46
Pksdyg <az5501262@qq.com>
2017-06-24 19:18:15 +0800

    移动+背景

--- js/Global.js
+++ js/Global.js
@@ -13,13 +13,15 @@ canv.style.marginTop = screen.height * 0.025;
 //砖长宽
 var brick_side = canvW / 20;
 var context = canv.getContext('2d');
-var capReady = false;
 var capImage = new Image();
 capImage.src = "img/cap.png";
+var bgImage = new Image();
+bgImage.src = "img/background.jpg";
 var cap;
 cap = new Object();
 cap.x = 0;
 cap.y = canvH - brick_side * 2;
+cap.speed = 256;
 
 function paint_brick(x, y) {
 	context.rect(x, y, brick_side, brick_side);
@@ -30,27 +32,34 @@ function paint_brick(x, y) {
 	context.stroke();
 }
 var w = window;
+var then = Date.now();
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+var keysDown = {};
+addEventListener("keydown", function(e) {
+	keysDown[e.keyCode] = true;
+}, false);
+addEventListener("keyup", function(e) {
+	delete keysDown[e.keyCode];
+}, false);
+var update = function(modifier) {
+	if(37 in keysDown) {
+		cap.x -= cap.speed * modifier;
+	}
+	if(39 in keysDown) {
+		cap.x += cap.speed * modifier;
+	}
+};
 var main = function() {
 	context.clearRect(0, 0, canvW, canvH);
+//	context.drawImage(bgImage, 0, 0);
+//	for(var i = 0; i < 22; i++) {
+//		paint_brick(i * brick_side, canvH - brick_side);
+//	}
 	context.drawImage(capImage, cap.x, cap.y);
-	for(var i = 0; i < 22; i++) {
-		paint_brick(i * brick_side, canvH - brick_side);
-	}
 	requestAnimationFrame(main);
+	var now = Date.now();
+	var delta = now - then;
+	update(delta / 1000);
+	then = now;
 };
 main();
-
-var capadd = function() {
-	cap.x += 1;
-	setTimeout("capadd()", 5);
-};
-document.onkeydown = function(event) {
-	var e = event || window.event || arguments.callee.caller.arguments[0];
-	if(e && e.keyCode == 37) {
-		cap.x-=5;
-	}
-	if(e && e.keyCode == 39) {
-		cap.x+=5;
-	}
-};
