adafa79002091f356f87ad0f1bfd1373712bb0e5
Pksdyg <az5501262@qq.com>
2017-06-27 18:10:22 +0800

    完美

--- js/Global.js
+++ js/Global.js
@@ -6,19 +6,17 @@ window.onload = function() {
 //定义画布属性
 var canv = document.getElementById('canv');
 var canv_bg = document.getElementById('canv_bg');
-var canvW = screen.width * 0.8;
-var canvH = screen.height * 0.8;
-var gameW = canvW * 2;
-var gameH = canvH * 2;
-var nowW = canvW / 2;
-var nowH = canvH / 2;
+var canvW = screen.width * 0.7;
+var canvH = screen.height * 0.7;
+var gameW = canvW * 3;
+var gameH = canvH * 3;
 canv.width = canvW;
 canv.height = canvH;
 canv_bg.width = canvW;
 canv_bg.height = canvH;
-canv.style.marginLeft = screen.width * 0.1;
+canv.style.marginLeft = screen.width * 0.15;
 canv.style.marginTop = screen.height * 0.025;
-canv_bg.style.marginLeft = screen.width * 0.1;
+canv_bg.style.marginLeft = screen.width * 0.15;
 canv_bg.style.marginTop = screen.height * 0.025;
 var conv_p = canv.getContext('2d');
 var conv_bg_p = canv_bg.getContext('2d');
@@ -38,19 +36,19 @@ brickImage.onload = function() {
 	brickReady = true;
 };
 brickImage.src = "img/砖.png";
-brickImage.width = screen.width * 0.06;
-brickImage.height = screen.width * 0.02;
+brickImage.width = canvW / 14;
+brickImage.height = canvW / 42;
 //定义背景
 var bgImage = new Image();
 bgImage.src = "img/background.jpg";
 var cap;
 cap = new Object();
-cap.x = canvH / 2 + brickImage.width * 2;
-cap.y = canvH - brickImage.height* 2.8;
-cap.speed = 256;
+cap.x = canvW + brickImage.width * 2;
+cap.y = canvH - brickImage.height * 2.8;
+cap.speed = 956;
 //画砖头的方法
 function paint_brick(x, y) {
-	conv_bg_p.drawImage(brickImage,x, y, brickImage.width, brickImage.height);
+	conv_bg_p.drawImage(brickImage, x, y, brickImage.width, brickImage.height);
 }
 //控制
 var keysDown = {};
@@ -62,20 +60,20 @@ addEventListener("keyup", function(e) {
 }, false);
 var update = function(modifier) {
 	if(37 in keysDown) { //left
-		cap.x -= cap.speed * modifier;
-		nowW += cap.speed * modifier;
+		if(cap.x > 0)
+			cap.x -= cap.speed * modifier;
 	}
 	if(39 in keysDown) { //right
-		cap.x += cap.speed * modifier;
-		nowW -= cap.speed * modifier;
+		if(cap.x <= gameW - capImage.width)
+			cap.x += cap.speed * modifier;
 	}
 	if(38 in keysDown) { //up
-		cap.y -= cap.speed * modifier;
-		nowH += cap.speed * modifier;
+		if(cap.y > 0)
+			cap.y -= cap.speed * modifier;
 	}
 	if(40 in keysDown) { //down
-		cap.y += cap.speed * modifier;
-		nowH -= cap.speed * modifier;
+		if(cap.y <= gameH - capImage.height)
+			cap.y += cap.speed * modifier;
 	}
 };
 //main函数
@@ -83,32 +81,40 @@ var w = window;
 var then = Date.now();
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 var main = function() {
-	conv_p.clearRect(0, 0, canvW, canvH);
-	conv_bg_p.clearRect(0, 0, canvW, canvH);
-	//conv_p.drawImage(bgImage, nowW - canvW / 2, nowH - canvH / 2);
-	if(capReady) {
-		conv_p.drawImage(capImage, cap.x + nowW - canvW / 2, cap.y + nowH - canvH / 2, capImage.width, capImage.height);
-	}	
-	for(var i = 0; i < 22; i++) {
-		paint_brick(i * brickImage.width + nowW - canvW / 2, canvH - brickImage.height + nowH - canvH / 2);
-	}
-	requestAnimationFrame(main);
 	var now = Date.now();
 	var delta = now - then;
 	update(delta / 1000);
 	then = now;
+	conv_p.clearRect(0, 0, gameW, gameH);
+	conv_bg_p.clearRect(0, 0, gameW, gameH);
+	//conv_p.drawImage(bgImage, nowW - canvW / 2, nowH - canvH / 2); 	
+	if(cap.x <= canvW * 2.5 && cap.x >= canvW * 0.5 || cap.y <= canvH * 2.5 && cap.y >= canvH * 0.5) {
+		conv_p.restore();
+		conv_bg_p.restore();
+		conv_p.save();
+		conv_bg_p.save();
+		if(cap.x <= canvW * 2.5 && cap.x >= canvW * 0.5) {
+			conv_p.translate(-cap.x + canvW / 2, 0);
+			conv_bg_p.translate(-cap.x + canvW / 2, 0);
+		} else if(cap.y <= canvH * 2.5 && cap.y >= canvH * 0.5 && cap.x >= canvW * 2.5) {
+			conv_p.translate(-canvW * 2, 0);
+			conv_bg_p.translate(-canvW * 2, 0);
+		}
+		if(cap.y <= canvH * 2.5 && cap.y >= canvH * 0.5) {
+			conv_p.translate(0, -cap.y + canvH / 2);
+			conv_bg_p.translate(0, -cap.y + canvH / 2);
+		} else if(cap.x <= canvW * 2.5 && cap.x >= canvW * 0.5 && cap.y >= canvH * 2.5) {
+			conv_p.translate(0, -canvH * 2);
+			conv_bg_p.translate(0, -canvH * 2);
+		}
+	}
+	if(capReady) {
+		conv_p.drawImage(capImage, cap.x, cap.y, capImage.width, capImage.height);
+	}
+	for(var i = 0; i < 30; i++) {
+		paint_brick(i * brickImage.width, canvH - brickImage.height);
+	}
+
+	requestAnimationFrame(main);
 };
 main();
-//鼠标事件(没用到)
-function gete(e) {
-	if(e.offsetX > canvW / 2) {
-		if(nowW > 0)
-			nowW--;
-	} else if(nowW < gameW)
-		nowW++;
-	if(e.offsetY > canvH / 2) {
-		if(nowH > 0)
-			nowH--;
-	} else if(nowH < gameH)
-		nowH++;
-}
