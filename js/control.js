e82e779530d7bedfd9248fbec95f7408dbf36a1f
林嘉豪 <1143910315@qq.com>
2017-07-05 01:00:04 +0800

    control.js

--- js/control.js
+++ js/control.js
@@ -1,5 +1,25 @@
-////*********************************************
-////此JS文件负责：
-////	处理用户的输入
-////	角色位置、旋转等状态改变
-////*********************************************
+//
+//  control.js
+//
+//  处理用户的输入
+//  requestAnimationFrame的循环
+//
+//  Created by 林嘉豪 on 2017-07-05.
+//  Copyright 2017 林嘉豪. All rights reserved.
+//
+(function() {
+	var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame;
+	var rafPaint = function rafPaint() {
+		raf(rafPaint);
+	}
+	var needLoadNum= 1;
+	var Control = {
+
+	};
+	Control.prototype.loadFinished = function() {
+		if(--needLoadNum == 0) {
+			raf(rafPaint);
+		}
+	};
+	window.Control = Control;
+})();
