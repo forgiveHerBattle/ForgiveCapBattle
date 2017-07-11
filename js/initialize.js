09184b15f63e6ea16b385dcda926b6ef6b57743b
Pksdyg <az5501262@qq.com>
2017-07-11 23:03:26 +0800

    。。。。

--- js/initialize.js
+++ js/initialize.js
@@ -64,21 +64,106 @@
 //	};
 //	obj.loadScript(jsList.shift(), obj.callback);
 //})();
-(function () {
-	var role1=new Role(5,3);
-	var role2=new Role(7,4);
-	role1.show();
-	role2.show();
-	role1.create();
+(function() {
+	//设置画布大小，定义画布属性
+	document.body.scroll = "no";
+	var canv = document.getElementById('canv');
+	var canvW = screen.width * 0.7;
+	var canvH = screen.height * 0.7;
+	var gameW = canvW * 3;
+	var gameH = canvH * 3;
+	canv.width = canvW;
+	canv.height = canvH;
+	canv.style.marginLeft = screen.width * 0.15;
+	canv.style.marginTop = screen.height * 0.025;
+	var conv_p = canv.getContext('2d');
+	//定义帽子
+	var capImage = new Image();
+	var capReady = false;
+	capImage.onload = function() {
+		capReady = true;
+	};
+	capImage.src = "img/cap.png";
+	capImage.width = screen.width * 0.06;
+	capImage.height = screen.width * 0.04;
+	//定义砖头
+	var brickImage = new Image();
+	var brickReady = false;
+	brickImage.onload = function() {
+		brickReady = true;
+	};
+	brickImage.src = "img/砖.png";
+	brickImage.width = canvW / 14;
+	brickImage.height = canvW / 42;
+
+	var cap;
+	cap = new Object();
+	cap.x = canvW + brickImage.width * 2;
+	cap.y = canvH - brickImage.height * 2.8;
+	cap.speed = 956;
+	//画砖头的方法
+	function paint_brick(x, y) {
+		conv_p.drawImage(brickImage, x, y, brickImage.width, brickImage.height);
+	}
+	//控制
+	var keysDown = {};
+	addEventListener("keydown", function(e) {
+		keysDown[e.keyCode] = true;
+	}, false);
+	addEventListener("keyup", function(e) {
+		delete keysDown[e.keyCode];
+	}, false);
+	var update = function(modifier) {
+		if(37 in keysDown) { //left
+			if(cap.x > 0)
+				cap.x -= cap.speed * modifier;
+		}
+		if(39 in keysDown) { //right
+			if(cap.x <= gameW - capImage.width)
+				cap.x += cap.speed * modifier;
+		}
+		if(38 in keysDown) { //up
+			if(cap.y > 0)
+				cap.y -= cap.speed * modifier;
+		}
+		if(40 in keysDown) { //down
+			if(cap.y <= gameH - capImage.height)
+				cap.y += cap.speed * modifier;
+		}
+	};
+	//main函数
+	var w = window;
+	var then = Date.now();
+	requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+	var main = function() {
+		var now = Date.now();
+		var delta = now - then;
+		update(delta / 1000);
+		then = now;
+		conv_p.clearRect(0, 0, gameW, gameH);
+		if(cap.x <= canvW * 2.5 && cap.x >= canvW * 0.5 || cap.y <= canvH * 2.5 && cap.y >= canvH * 0.5) {
+			conv_p.restore();
+			conv_p.save();
+			if(cap.x <= canvW * 2.5 && cap.x >= canvW * 0.5) {
+				conv_p.translate(-cap.x + canvW / 2, 0);
+			} else if(cap.y <= canvH * 2.5 && cap.y >= canvH * 0.5 && cap.x >= canvW * 2.5) {
+				conv_p.translate(-canvW * 2, 0);
+			}
+			if(cap.y <= canvH * 2.5 && cap.y >= canvH * 0.5) {
+				conv_p.translate(0, -cap.y + canvH / 2);
+			} else if(cap.x <= canvW * 2.5 && cap.x >= canvW * 0.5 && cap.y >= canvH * 2.5) {
+				conv_p.translate(0, -canvH * 2);
+			}
+		}
+		if(capReady) {
+			conv_p.drawImage(capImage, cap.x, cap.y, capImage.width, capImage.height);
+		}
+		for(var i = 0; i < 30; i++) {
+			paint_brick(i * brickImage.width, canvH - brickImage.height);
+		}
+
+		requestAnimationFrame(main);
+	};
+	main();
+	var Me = new Role(5, 3);
 })();
-
-var x=function () {
-
-};
-x.prototype.x=0;
-x.prototype.y=0;
-var y;
-y.prototype=x;
-y.prototype.z=0;
-var z=new y();
-z.q=0;
--- js/role.js
+++ js/role.js
@@ -3,16 +3,13 @@
 ////	角色位置、动作等状态改变
 ////*********************************************
 (function() {
-	var z=0;
-	var Role = function(x1, y1) {
-		var x = 0;
-		var y = 0;
-		x = x1;
-		y = y1;
+	var x = 0;
+	var y = 0;
+	var z = 0;
+	var Role = function(x, y) {
+		this.x = x;
+		this.y = y;
 		z++;
-		this.show = function() {
-			console.log(x, y);
-		}
 	}
 	/**
 	 * @private
